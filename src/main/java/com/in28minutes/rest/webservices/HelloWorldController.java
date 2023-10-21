24233e24a5aaf6fd08632bf6f34eda4b3ad5ddef
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-10-21 12:38:10 +0700

    update

--- src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
+	@GetMapping()
+	public String homePage() {
+		return "Home Page";
+	}
+	
 	@GetMapping(value = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
